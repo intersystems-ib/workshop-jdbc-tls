7718a239b772cfa0058436343c3ac8de816d2189
isc-lperezra <lperezra@intersystems.com>
2023-04-14 23:32:08 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# JAVA JDBC and SSL/TLS connections
+## Prerequisites:
+
+To test this project is required to install:
+
+[Apache Maven](https://dlcdn.apache.org/maven/maven-3/3.9.1/binaries/apache-maven-3.9.1-bin.zip)
+[Windows Openssl](https://slproweb.com/download/Win64OpenSSL_Light-3_1_0.exe)
+[Java JDK 11](https://www.oracle.com/es/java/technologies/javase/jdk11-archive-downloads.html#license-lightbox)
+[Visual Studio Code](https://code.visualstudio.com/download)
+
+## Configuration:
+
+InterSystems jdbc library is not uploaded into Maven Repository so we have to install it into our local Maven repository. To do it we have to create c:\Certificados folder and copy **intersystems-jdbc-3.6.1.jar** into it. The next step is to execute the following command:
+
+```bash
+mvn install:install-file -Dfile=c:\\Certificados\\intersystems-jdbc-3.6.1.jar -DgroupId=com.intersystems -DartifactId=intersystems-jdbc -Dversion=3.6.1 -Dpackaging=jar
+```
+Please, confirm that a new folder has been created in the local repository (similar to `C:\Users\username\.m2\repository\com\intersystems\intersystems-jdbc\3.6.1`)
+
+This code is a Java project based on MAVEN so we just need to execute `mvn clean install` to confirm that we have installed all the dependencies of the project.
+
+Finally, we have to configure two environment variables in the system:
+1. JAVA_HOME: path to java JDK installation (bin folder excluded).
+2. OPENSSL_HOME: path to OpenSSL installation (bin folded included).
+
+### application.properties
+
+In this file we will configure the JDBC data to connect with our IRIS instance, please, review and update it to use your own instance.
+
+### SSLConfig.properties
+
+This file contains the parameters requested to establish SSL/TLS connections. No further changes are necessary.
+
+### Configuration.xml
+
+You will find in this file an eported objectscript class. The system will import it into your IRIS instance and create all the configurations to establish SSL/TLS connections automatically.
+
+## Running the project:
+
+Visual Studio Code detects automatically the type of the project, you only have to open **RUN** menu and click on **Run Without Debug**. The project is configured to create certificates and keys in c:\Certificados folder and configure the SSL/TLS connections in IRIS. When the configuration is finished a connection is opened with IRIS and a SQL query is executed. 
+
+ATTENTION! the query is configured to insert a record in a table located in PHONEBOOK namespace. You should modify it in order to use a namespace and a table of your IRIS instance.
---
+++ src/main/java/com/certificate/iristls/IristlsApplication.java
@@ -0,0 +1,98 @@
+package com.certificate.iristls;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.intersystems.jdbc.IRIS;
+import com.intersystems.jdbc.IRISConnection;
+import com.intersystems.jdbc.IRISDataSource;
+
+@SpringBootApplication
+public class IristlsApplication implements CommandLineRunner{
+
+	@Value( "${spring.datasource.username}" )
+	private String username;
+	@Value( "${spring.datasource.password}" )
+	private String password;
+	@Value( "${spring.datasource.url}" )
+	private String url;
+	@Value( "${spring.datasource.sys.url}" )
+	private String sysUrl;
+
+	public static void main(String[] args) {
+		SpringApplication.run(IristlsApplication.class, args);
+	}
+
+	@Override
+    public void run(String... args) throws Exception {
+
+		try{
+    		String javaHome = System.getenv("JAVA_HOME");
+			String opensslHome = System.getenv("OPENSSL_HOME");
+			
+			// Certificate and private key generation
+			Process p = Runtime.getRuntime().exec("\""+opensslHome+"\\openssl\" req -x509 -newkey rsa:2048 -keyout C:\\Certificados\\key.pem -out C:\\Certificados\\cert.pem -sha256 -days 3650 -nodes -subj \"/CN=localhost\"");
+			p.waitFor();
+			
+			// Deletion of old truststore just in case
+			File truststore = new File("C:\\Certificados\\truststore.jks");
+			
+			if (truststore.exists()){
+				truststore.delete();
+			}		
+			// Creation of new truststore with certification fingerprint
+			p = Runtime.getRuntime().exec("\""+javaHome+"\\bin\\keytool\" -importcert -file C:\\Certificados\\cert.pem -keystore C:\\Certificados\\truststore.jks -storepass InterSystems -noprompt -trustcacerts");
+			p.waitFor();
+			
+			File folder = new File("src/main/resources");
+			String absolutePath = folder.getAbsolutePath();
+			
+			// IRIS CONNECTION
+			IRISConnection irisConnection =  new IRISConnection();
+			irisConnection = (IRISConnection) DriverManager.getConnection(sysUrl, username, password);
+			IRIS irisInstance = IRIS.createIRIS(irisConnection);
+
+			// Execution de IRIS commands to create %SuperServer TLS/SSL configuration and enable TLS/SSL comunication for SuperServer
+			boolean superServerConfigLoaded = irisInstance.classMethodBoolean("%SYSTEM.OBJ", "Load", absolutePath+"\\Configuration.xml","ck","","1");
+			boolean sslSuperServerUpdated = irisInstance.classMethodBoolean("User.Configuration", "UpdateSSLSuperServer");
+			
+			if (superServerConfigLoaded && sslSuperServerUpdated)
+			{
+				// Example of insert in specific table and namespace, to reproduce the example is required to configure an existing namespace and table
+				IRISDataSource ds = new IRISDataSource();
+				ds.setConnectionSecurityLevel(10);
+				ds.setURL(url);
+				ds.setUser(username);
+				ds.setPassword(password);
+				// ds.setKeyRecoveryPassword(keyPassword);
+				Connection conn = ds.getConnection();
+				String insert = "INSERT INTO Phonebook.Company VALUES(?,?,?)";
+				try {
+					PreparedStatement ps = conn.prepareStatement(insert);
+					ps.setNull(1, -5    );
+					ps.setString(2, "An address" );
+					ps.setObject(3, "A company"   );
+					ps.executeUpdate();
+					ps.close();
+				}
+				catch (Exception e) {
+					System.out.println("\nException in insert\n"+e);
+				}
+				irisConnection.close();
+				conn.close();
+			}
+		  }
+		catch (SQLException e){
+		  System.out.println(e.getMessage());
+		  e.printStackTrace();
+		}
+	}
+}
